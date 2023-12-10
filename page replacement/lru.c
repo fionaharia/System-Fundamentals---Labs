eebcf53b49b91f1ba15778ece1f989d2722a078e
Fiona Haria <hariafiona@gmail.com>
2023-12-10 13:45:52 +0530

    modifed lru

--- page replacement/lru.c
+++ page replacement/lru.c
@@ -1,101 +1,101 @@
 #include<stdio.h>
-void lru_algo(int pageref[],int frames,int pages);
-int main()
+#include<limits.h>
+
+
+int checkHit(int incomingPage,int queue[],int occupied);
+void printFrame(int queue[],int occupied);
+
+int checkHit(int incomingPage,int queue[],int occupied)
 {
-      int frames,pages,i;
-      printf("Enter the number of frames: \n");
-      scanf("%d",&frames);
-      printf("Enter the number of pages: \n");
-      scanf("%d",&pages);
-      int pageref[pages];
-      printf("Enter %d page reference elements: \n",pages);
-      for(i=0;i<pages;i++)
-            scanf("%d",&pageref[i]);
-      
-      lru_algo(pageref,frames,pages);
+      for(int i=0;i<occupied;i++)
+      {
+            if(incomingPage==queue[i])
+            {
+                  return 1;
+            }
+      }
       return 0;
 }
 
-void lru_algo(int pageref[],int frames,int pages)
+void printFrame(int queue[],int occupied)
 {
-      int framearr[frames];
-      int i,j;
-      for(i=0;i<frames;i++)
+      for(int i=0;i<occupied;i++)
       {
-            framearr[i] = -1;
-
+            printf("%d\t\t",queue[i]);
       }
-      int index=-1,miss=0,hits=0;
-
-      for(i=0;i<pages;i++)
-      {
-            int flag=0;
-            int full = 0;
-            int symbol = pageref[i];
-            for(j=0;j<frames;j++)
-            {
-                  if(symbol==framearr[j])
-                  {
-                        flag = 1;
-                        break;
-                  }
-            }
-            
-            if(flag==1)
-            {
-                  printf("\nSymbol: %d Frame: ",symbol);
-                  for(j=0;j<frames;j++)
-                  {
-                        printf("%d ",framearr[j]);
-                  }
-                  hits++;
-            }
-            else
-            {
-                  if(full==0)
-                  {
-                        index=(index+1)%frames;
-                        framearr[index]=symbol;
-                        miss++;
-                        for(j=0;j<frames;j++)
-                        {
-                              printf("%d ",framearr[j]);
-                        }
-                  }
-
-                  else
-                  {
-                        int pos=999;
-                        int index=-1;
-                        for(j=0;j<frames;j++)
-                        {
-                              for(int k=0;k<pages;k++)
-                              {
-                                    if(framearr[j]==pageref[k])
-                                    {
-                                          if(pos>k)
-                                          {
-                                                pos = k;
-                                                index = j;
-                                                break;
-                                          }
-                                    }
-                              }
-                        }
-                        framearr[index] = symbol;
-                        miss++;
-                        printf("\nSymbol: %d Frame: ",symbol);
-                        for(j=0;j<frames;j++)
-                        {
-                              printf("%d ",framearr[j]);
-                        }
-                  }
-            }
-      }
-      printf("\nPage Hits: %d",hits);
-      printf("\nPage Misses: %d",miss);  
-
 }
 
+int main()
+{
+      int frames, n;
+      printf("Enter the number of frames: ");
+      scanf("%d", &frames);
+      printf("Enter the number of pages: ");
+      scanf("%d", &n);
 
+      int incomingStream[n];
+      printf("Enter the page sequence:\n");
+      for (int i = 0; i < n; i++)
+      {
+            printf("Page %d: ", i + 1);
+            scanf("%d", &incomingStream[i]);
+      }
 
+      int queue[frames];
+      int distance[frames];
+      int occupied = 0;
+      int miss = 0;
+      int hit=0;
+
+      printf("Pages\n");
+
+      for(int i=0;i<n;i++)
+      {
+            printf("%d\t\t",incomingStream[i]);
+            if(checkHit(incomingStream[i],queue,occupied))
+            {
+                  printFrame(queue,occupied);
+                  hit++;
+            }
+
+            else if(occupied<frames)
+            {
+                  queue[occupied] = incomingStream[i];
+                  miss++;
+                  occupied++;
+                  printFrame(queue,occupied);
+            }
+
+            else
+            {
+                  int max = INT_MIN;
+                  int index;
+
+                  for(int j=0;j<frames;j++)
+                  {
+                        distance[j] = 0;
+                        for(int k=i-1;k>=0;k--)
+                        {
+                              ++distance[j];
+                              if(incomingStream[k]==queue[j])
+                                    break;
+                        }
+                        if(distance[j]>max)
+                        {
+                              max = distance[j];
+                              index = j;
+                        }
+                  }
+
+                  queue[index] = incomingStream[i];
+                  printFrame(queue,occupied);
+                  miss++;
+            }
+            printf("\n");
+
+      }
+
+      printf("Page Hits: %d \n",hit);
+      printf("Page Miss: %d \n",miss);
+      return 0;
+}
